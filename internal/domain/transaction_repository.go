9c0c8e3c3480ca116fdb3734dcf3747d7ea67270
mrazriel12 <azrielaja870@gmail.com>
2026-06-11 17:54:07 +0700

    Clean up package imports in transaction_repository
    Removed unnecessary package declarations to streamline code.

--- internal/domain/transaction_repository.go
+++ internal/domain/transaction_repository.go
@@ -1,13 +1,8 @@
-package domain
-
-import "context"
-
 type TransactionRepository interface {
 	Create(ctx context.Context, tx *TransactionCreate) (string, error)
 	GetByTxID(ctx context.Context, txID string) (*TransactionDetail, error)
 	GetUserBalance(ctx context.Context, userID int64) (*UserBalance, error)
 	GetUserTransactions(ctx context.Context, userID int64, limit int, offset int) ([]*TransactionDetail, error)
-}
 	GetAccountBalance(ctx context.Context, accountNo string) (*AccountBalance, error)
 	GetAccountTransactions(ctx context.Context, accountNo string, limit int, offset int) ([]*TransactionDetail, error)
 }
